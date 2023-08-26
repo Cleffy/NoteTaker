7a21a7ce46de8cd79d1dfdb51810899baf6105e2
Cleffy <falchard@live.com>
2023-08-26 00:57:58 -0700

    Setup notes router

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 const env = require('dotenv');
 
-//Initiate express
+// Initiate express
 const app = express();
 
-//Specify port
+// Specify port
 const PORT = process.env.PORT || 5000;
+
+// Middlewares
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Start server
+// Notes API Routes
+app.use('/api/notes', require('./routes/api/notes'));
+
+// Start server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
